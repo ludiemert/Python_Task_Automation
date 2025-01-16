0c5e20ce398729cea75dc3e19a269dbd7b8f697e
ludiemert <lucianadiemert@gmail.com>
2025-01-16 15:40:48 -0300

    add comand project and install lib pandas

---
+++ auxiliar.py
@@ -0,0 +1,11 @@
+# saber  posicao x e y do mouse em valor e seguir o programa
+
+import pyautogui
+import time # para o comp esperar eu colocar o mouse
+
+time.sleep(5) #espera 5 segund ate eu colocar o mouse em cima do campo login para eu saber a posicao x e y do login
+
+# pegar a posicao do mouse em tela do note
+print(pyautogui.position()) #print ele mostra a posicao do seu mouse do momento em print
+# pegando a posicao Point(x=466, y=374) colocar no projeto
+
--- code_1.py
+++ code_1.py
@@ -37,11 +37,79 @@ time.sleep(3) # seu comp vai ficar "dormindo" qto tempo vc quiser, depois vai p 
 
 # Passo 2:   Fazer login
 # tem que clicar no campo de email e selecionar, p isso vc precisa dar click com mouse na tela, o lugar da tela??
-
-
+pyautogui.click(x=466, y=374)
+# se quiser dar mais comandos na mesma linha pyautogui.click(x=466, y=374, clicks=2)
+#digitar o email
+pyautogui.write("lua@gmail.com")
+#passar para prox linha senha (tem 2 formas ou pega posicao do mouse ou tecla TAB ele vai p prox linha)
+pyautogui.press("tab")
+#senha
+pyautogui.write("123")
+#prox tecla e depois de enter p entrar no sistema
+pyautogui.press("tab")
+#tecla login
+#prox tecla e depois de enter p entrar no sistema
+pyautogui.press("enter")
 
 # Passo 3: Importar a base de dados dos produtos
+#instal  pip install pandas openpyxl
+import pandas
+
+# precisa armazenar essa base de dados, preciso criar uma variavel
+# vai ler e armazenar na variavel tabela
+
+tabela = pandas.read_csv("produtos.csv")
+
+print(tabela)
+
+time.sleep(2)
+# trazer a base de dados para o pandas
+#ler um arquivo csv
+# pandas.read_csv("produtos.csv") #passa o produto que quer ler
+# se quiser ver a tabela print(tabela)
+# se quiser copiar tabela tabela.copy()
 
 # Passo 4 : Cadastrar 1 produto
+#pegar posicao x,y do cod produto
+pyautogui.click(x=463, y=257)
+
+#cadastrar o texto na primeira linha, o primeiro produto
+#fazer 1 produto primeiro
+# MOLO000251,Logitech,Mouse,1,25.95,6.50,
+# nome das colunas => codigo,marca,tipo,categoria,preco_unitario,custo,obs
+
+#codigo
+pyautogui.write("MOLO000251")
+pyautogui.press("tab")
+
+#marca
+pyautogui.write("Logitech")
+pyautogui.press("tab")
+
+#tipo
+pyautogui.write("Mouse")
+pyautogui.press("tab")
+
+#categoria
+pyautogui.write("1")
+pyautogui.press("tab")
+
+#preco_unitario
+pyautogui.write("25.95")
+pyautogui.press("tab")
+
+#custo
+pyautogui.write("6.50")
+pyautogui.press("tab")
+
+#obs
+pyautogui.write("")
+pyautogui.press("tab")
+
+
+#Enviar
+
+
+
 
 # Passo 5: Repetir o passo 4 ate acabar todos os produtos
