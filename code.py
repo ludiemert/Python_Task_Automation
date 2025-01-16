af3b8d4959ff8141b67dfb479ddb57a852284786
ludiemert <lucianadiemert@gmail.com>
2025-01-16 13:19:19 -0300

    add version py and instal lib pyautogui

---
+++ code.py
@@ -0,0 +1,11 @@
+# Passo 1: Abrir o sistema da empresa
+  #sistema :  https://dlp.hashtagtreinamentos.com/python/intensivao/login
+
+  
+# Passo 2:   Fazer login 
+
+# Passo 3: Importar a base de dados dos produtos
+
+# Passo 4 : Cadastrar 1 produto 
+
+# Passo 5: Repetir o passo 4 ate acabar todos os produtos
